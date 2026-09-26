3ef810754db2ee2ae4145c2541d07270bf0c20e3
Aeb03 <bestosoadrian33@gmail.com>
2026-09-26 01:12:41 -0300

    v0.7.0 - interfaz consolidada experimental

--- brand.js
+++ brand.js
@@ -1,5 +1,5 @@
 (()=>{'use strict';
-const V='0.6.1';
+const V='0.7.0';
 function rep(){
   const w=document.createTreeWalker(document,NodeFilter.SHOW_TEXT),a=[];
   while(w.nextNode())a.push(w.currentNode);
